a1c23413f04265508decdf4bb48133c9520047f5
Pax Newman <pax.newman@gmail.com>
2023-08-01 14:53:40 -0700

    moved api logic to its own directory

---
+++ app/api.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from enum import Enum
+
+from sentence_transformers import SentenceTransformer
+
+# def get_models(model_type: str, args: Namespace) -> type:
+#     path = args.model_paths.model_type
+#
+#     new_type = type(model_type, (str, Enum), {})
+#
+#     for model in os.listdir(path):
+#         setattr(new_type, model, model)
+#
+#     return new_type
+#
+app = FastAPI()
+
+@app.get('/')
+async def root():
+    return {'message' : 'hello world'}
+
+# Keep most recently used model in memory to avoid loading it at each request
+app.state.model = None
+app.state.model_name = ""
+
+def set_model(model_name):
+    if app.state.model_name != model_name:
+        if model_name in TextEmbeddingModels:
+            app.state.model = SentenceTransformer(model_name)
+
+        app.state.model_name = model_name
+
+# --- Text Embeddings --- #
+
+# TextEmbeddingModels = get_models('text_embedding', my_args)
+
+class TextEmbeddingModels(str, Enum):
+    all_minilm_l6_v2 = 'all-MiniLM-L6-v2'
+
+class TextEmbeddingRequest(BaseModel):
+    model: TextEmbeddingModels
+    input: str | list[str]
+
+@app.get('/embeddings/text/models')
+async def text_embedding_models():
+    return list(TextEmbeddingModels)
+
+@app.post('/embeddings/text')
+async def text_embeddings(req: TextEmbeddingRequest):
+    # TODO count tokens and return error if too many
+    
+    # Create Model
+    set_model(req.model)
+
+    # Embed text
+    features = app.state.model.encode(req.input)
+
+    # Convert embeddings to a list of floats
+    if isinstance(features, list):
+        data = [feature.tolist() for feature in features]
+    else:
+        data = features.tolist()
+
+    return data
+
+
--- main.py
+++ main.py
@@ -1,54 +1,6 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from enum import Enum
 
-from sentence_transformers import SentenceTransformer
+import uvicorn
+from app.api import app
 
-
-app = FastAPI()
-
-@app.get('/')
-async def root():
-    return {'message' : 'hello world'}
-
-# Keep most recently used model in memory to avoid loading it at each request
-app.state.model = None
-app.state.model_name = ""
-
-def set_model(model_name):
-    if app.state.model_name != model_name:
-        if model_name in TextEmbeddingModels:
-            app.state.model = SentenceTransformer(model_name)
-
-        app.state.model_name = model_name
-
-# --- Text Embeddings --- #
-
-class TextEmbeddingModels(str, Enum):
-    all_minilm_l6_v2 = 'all-MiniLM-L6-v2'
-
-class TextEmbeddingRequest(BaseModel):
-    model: TextEmbeddingModels
-    input: str | list[str]
-
-@app.get('/embeddings/text/models')
-async def text_embedding_models():
-    return list(TextEmbeddingModels)
-
-@app.post('/embeddings/text')
-async def text_embeddings(req: TextEmbeddingRequest):
-    # Create Model
-    set_model(req.model)
-
-    # Embed text
-    features = app.state.model.encode(req.input)
-
-    # Convert embeddings to a list of floats
-    if isinstance(features, list):
-        data = [feature.tolist() for feature in features]
-    else:
-        data = features.tolist()
-
-    return data
-
-
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
