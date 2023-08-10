1e70d8bc2363082d959fb9cc0c7afb7c40c27e60
Pax Newman <pax.newman@gmail.com>
2023-08-10 15:55:01 -0700

    fixed enum injection

--- app/utils.py
+++ app/utils.py
@@ -22,6 +22,7 @@ def get_app_instance(request: Request) -> FastAPI:
     return request.app
 
 def inject_models_to_enum(enum_locals: dict, values: dict):
+    values = values.__dict__
     # Dynamically fill an enum with values
     for key, value in values.items():
         enum_locals[key] = value.name
